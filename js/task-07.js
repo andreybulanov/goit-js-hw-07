659a5f46d60792d924de77e00a14258665ba75f6
Andrey Bulanov <bulanov1804@gmail.com>
2021-06-11 17:53:47 +0300

    7 задача

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,9 @@
 
 const input = document.querySelector('#font-size-control');
-nameLabel = document.querySelector('text');
+const nameLabel = document.querySelector('#text');
 
 input.addEventListener('input', onInputChange);
-function onInputChange(value) {
-  `${range.value}px`
+function onInputChange(event) {
+    nameLabel.style.fontSize = event.currentTarget.value +'px';
 }
 
